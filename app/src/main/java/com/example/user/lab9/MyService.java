dc4e11a72a983903947617cc6eed53aa78a8d97e
shark <wasdx1107@gmail.com>
2018-11-22 14:55:48 +0800

    fin

--- app/src/main/java/com/example/user/lab9/MyService.java
+++ app/src/main/java/com/example/user/lab9/MyService.java
@@ -5,12 +5,36 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class MyService extends Service {
-    public MyService() {
-    }
 
     @Override
     public IBinder onBind(Intent intent) {
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+    @Override
+    public void onCreate(){
+        new Thread(){
+            boolean flag =true;
+            int count =0;
+
+            @Override
+            public void run(){
+                while (flag){
+                    Intent i =new Intent("MyMessage");
+                    i.putExtra("background_service", count);
+                    sendBroadcast(i);
+                    count++;
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }.start();
+
     }
 }
